dac31388032095e6a494ec706be4241fe1899697
mahanfr <mahan.farzaneh@gmail.com>
2024-11-26 13:55:44 +0330

    Creating Login and logout page

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
 
 
 def register_view(request):
-    return render(request, "accounts/register.html", context={'form': UserRegisterForm})
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = UserRegisterForm()
+    return render(request, "accounts/register.html", context={'form': form})
--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from accounts import views as account_views
 
@@ -11,5 +12,7 @@ urlpatterns = [
 
 urlpatterns += i18n_patterns(
     path('', include('pages.urls')),
-    path('register', account_views.register_view),
+    path('register', account_views.register_view, name="register"),
+    path('login', auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
+    path('logout', auth_views.LogoutView.as_view(template_name="accounts/logout.html"), name="logout")
 )
